0bfee5347f4bccc5169dfe97a38c9d5095025ea8
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2018-12-19 14:47:11 +0530

    Add files via upload

---
+++ 19.RemoveNthNode.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(n<=0){
+            return head;
+        }
+        
+
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+    ListNode preDelete = dummy;
+
+    for (int i = 0; i < n; i++) {
+      if (head == null) {
+        return null;
+      }
+      head = head.next;
+    }
+
+    while (head != null) {
+      preDelete = preDelete.next;
+      head = head.next;
+    }
+
+    preDelete.next = preDelete.next.next;
+
+    return dummy.next;
+  }
+}
