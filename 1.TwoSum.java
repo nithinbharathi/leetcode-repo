fef813d07cd17c2a1db536fa8289577e0e20a3c8
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2018-12-19 18:11:58 +0530

    problem #1

--- 1.TwoSum.java
+++ 1.TwoSum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int nums[], int target) {
         HashMap<Integer,Integer>map = new HashMap<>();
         int result[] = new int[2];
         for(int i=0;i<nums.length;i++){
