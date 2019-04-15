2a02d65868d4a073309ebea98a4aa5c997d59f60
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-04-15 11:29:00 +0530

    Create SingleElementInSortedArray.java

---
+++ SingleElementInSortedArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end){
+            int mid = (start+end)/2;
+            if(mid%2 ==0){
+                if(nums[mid] == nums[mid+1]){
+                   start = mid+2; 
+                }else{
+                    end = mid-1;
+                }
+            }else{
+                if(nums[mid] == nums[mid-1]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return nums[start];
+    }
+}
