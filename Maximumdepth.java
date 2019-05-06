68e16853653b306f673e24f947ef942ed2049a51
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-06 14:30:59 +0530

    #Problem104

---
+++ Maximumdepth.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root  == null){
+            return 0;
+        }
+        int left  = 1+maxDepth(root.left);
+        int right = 1+maxDepth(root.right);
+        return Math.max(left,right);
+    }
+}
