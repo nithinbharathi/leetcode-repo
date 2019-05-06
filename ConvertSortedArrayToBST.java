f2c7bfb4afb45a69fe88da33d3ea381c79281bbe
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-06 20:34:19 +0530

    Update ConvertSortedArrayToBST.java

--- ConvertSortedArrayToBST.java
+++ ConvertSortedArrayToBST.java
@@ -20,3 +20,38 @@ class Solution {
         return root;
     }
 }
+
+/*A more iterative approach 
+using start and end pointers just like binary search*/
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0){
+            return null;
+        }
+        return helper(nums,0,nums.length-1);
+        
+    }
+    public static TreeNode helper(int arr[],int start,int end){
+        if(end<start){
+           return null; 
+        }
+        int mid = (start+end)/2;
+        TreeNode node = new TreeNode(arr[mid]);
+        node.left = helper(arr,start,mid-1);
+        node.right = helper(arr,mid+1,end);
+        return node;
+    }
+}
