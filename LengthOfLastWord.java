805649bace07f17407986836daf91e9a28104c1f
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-19 11:50:23 +0530

    #Problem 58

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        if(s.length() == 0 || s == null){
+        if(s.length() == 0||s == null){
             return 0;
         }
         int count =0;
