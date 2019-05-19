5d504d6dca2f1589d493c5d06f62519175343bc4
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-19 11:49:41 +0530

    #Problem 52

---
+++ LengthOfLastWord.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s.length() == 0 || s == null){
+            return 0;
+        }
+        int count =0;
+        String str = s.trim();
+        int last = str.length()-1;
+        while(last >=0 && str.charAt(last) != ' '){
+            count++;
+            last--;
+        }
+        
+        return count;
+    }
+}
