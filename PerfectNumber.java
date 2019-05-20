06d1cda279772f170a6292febb8081aa1386e714
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-20 20:43:19 +0530

    #Problem 507

---
+++ PerfectNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 1;
+        if(num == 1 && num ==1)return false;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i == 0){
+                sum += i + (num/i);
+            }
+        }
+        if(sum == num){
+            return true;
+        }
+        return false;
+    }
+}
