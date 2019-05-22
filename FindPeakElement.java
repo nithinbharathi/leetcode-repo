ee0d47765701ca431171ec27c468304bdfefd1ae
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-05-22 09:56:21 +0530

    #Problem 162

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int peak = 0;
-        int i = 0;
+        int i =0;
      for(i=1;i<nums.length;i++){
          if(nums[peak] < nums[i]){
              peak = i;
