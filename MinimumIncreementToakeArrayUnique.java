d4fcc05672931b889080a98d60196a3a272d72fc
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-06-22 13:13:02 +0530

    #Problem 945

---
+++ MinimumIncreementToakeArrayUnique.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minIncrementForUnique(int[] A) {
+        Arrays.sort(A);
+        int result =0;
+        for(int i = 1;i<A.length;i++){
+            if(A[i] <= A[i-1]){
+                result += A[i-1]+1-A[i];
+                A[i] = A[i-1]+1;
+            }
+        }
+        return result;
+    }
+}
