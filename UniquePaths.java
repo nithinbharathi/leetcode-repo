b58b7e29ddf00ccb58b726486a57647125b7355d
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-08-17 17:09:39 +0530

    Update UniquePaths.java

--- UniquePaths.java
+++ UniquePaths.java
@@ -17,3 +17,28 @@ class Solution {
         return dp[i][j];
     }
 }
+
+
+//Bottom-Up Approach
+//Time Complexity: O(m*n)
+//Space Complexity: o(m*n)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int i =0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        for(int i =0;i<m;i++){
+            dp[i][0] = 1;
+        }
+         for(int i = 1;i<m;i++){
+             for(int j =1;j<n;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+             }
+         }
+        return dp[m-1][n-1];
+        
+    }
+  
+}
